ea97781a26cadf755ce32534b1af8e1cb7f6a228
AndresArias07 <s.andresarias24@gmail.com>
2026-03-12 12:17:13 +0100

    Second commit

--- assets/js/superplus-for-woocommerce.js
+++ assets/js/superplus-for-woocommerce.js
@@ -1,34 +1,19 @@
-jQuery(function($){
+jQuery(function ($) {
   function getProUrl() {
-    var url = (typeof SP_WSV_Admin !== "undefined" && SP_WSV_Admin && SP_WSV_Admin.pro_url) ? SP_WSV_Admin.pro_url : "";
+    var url =
+      typeof SP_WSV_Admin !== "undefined" &&
+      SP_WSV_Admin &&
+      SP_WSV_Admin.pro_url
+        ? SP_WSV_Admin.pro_url
+        : "";
     url = (url || "").toString().trim();
     return url;
   }
 
-  $("body").on("click", ".sp-wsv-go-pro-popup-trigger, #toplevel_page_sp-wsv a[href*=\"page=sp-wsv-get-pro\"]", function(e){
+  $("body").on("click", ".sp-wsv-go-pro-popup-trigger", function (e) {
     e.preventDefault();
     var url = getProUrl();
     if (!url) return;
-
-    let btn = $("<a/>")
-      .text("Obtén versión PRO")
-      .addClass("sp-wsv-go-pro-link")
-      .attr("href", url)
-      .attr("target", "_blank")
-      .attr("rel", "noopener noreferrer")
-      .prop("outerHTML");
-
-    window.crear_sp_popup("go-pro-popup", "Obtén versión PRO", btn);
-  });
-
-  $("body").on("click", ".sp-wsv-pro-banner.is-dismissible .notice-dismiss", function(){
-    if (typeof SP_WSV_Admin === "undefined") return;
-    if (!SP_WSV_Admin || !SP_WSV_Admin.ajax_url || !SP_WSV_Admin.nonce) return;
-    $.post(SP_WSV_Admin.ajax_url, {
-      action: "sp_wsv_dismiss_pro_banner",
-      nonce: SP_WSV_Admin.nonce
-    });
+    window.open(url, "_blank", "noopener,noreferrer");
   });
 });
-
-
--- includes/admin/views/modules-page.php
+++ includes/admin/views/modules-page.php
@@ -9,7 +9,7 @@ $dismissed = (int) get_user_meta(get_current_user_id(), 'sp_wsv_comunidad_dismis
     <?php if (!$dismissed) { ?>
         <div id="sp-comunidad-alert" class="alert-comunidad">
             <span>¡Únete a la comunidad Tribu WP! Construye la solucion que tu cliente necesita.</span>
-            <a href="<?= esc_url('https://chat.whatsapp.com/DGiuqztlTlnAYalWnRTcQt?mode=gi_t') ?>" class="btn btn-whatsapp">
+            <a href="<?= esc_url('https://chat.whatsapp.com/DGiuqztlTlnAYalWnRTcQt?mode=gi_t') ?>" target="_blank" class="btn btn-whatsapp">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="currentColor">
                     <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347z" />
                     <path d="M12 0C5.373 0 0 5.373 0 12c0 2.124.558 4.127 1.532 5.864L.057 23.571a.75.75 0 0 0 .921.921l5.764-1.473A11.955 11.955 0 0 0 12 24c6.627 0 12-5.373 12-12S18.627 0 12 0zm0 22c-1.907 0-3.686-.523-5.205-1.431l-.364-.216-3.768.964.98-3.7-.236-.374A9.96 9.96 0 0 1 2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10z" />
@@ -206,7 +206,7 @@ $dismissed = (int) get_user_meta(get_current_user_id(), 'sp_wsv_comunidad_dismis
 
                 <div class="sp-wsv-card is-locked">
                     <div class="sp-wsv-card-icon">
-                        <img src="<?php echo esc_url(SP_WSV_URL . 'includes/modules/checkout-steps/assets/img/sello-recomendado.webp'); ?>" alt="" />
+                        <img src="<?php echo esc_url(SP_WSV_URL . 'includes/modules/assets/img/sello-recomendado.webp'); ?>" alt="" />
                     </div>
 
                     <div class="sp-wsv-card-top">
